1a614e652e033023eca5963b926a21e83efda104
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-05-28 15:37:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,35 +4,32 @@ import React, { useState, useEffect } from "react";
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
-  const [preview, setPreview] = useState('');
-  const [quote, setQuote] = useState('');
-  
-  const handleKeyPress = (keyValue) => {
-    setPreview(preview + keyValue);
-  };
-  
-  useEffect(() => {
-    if(preview === 'forty two'){
-      fetch('https://api.quotable.io/random')
-      .then(response => response.json())
-      .then(data => {
-        setQuote(data.content);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    } else{
-      setQuote('');
-    }
-  },[preview]);
+    const [preview, setPreview] = useState("");
+    const [quote, setQuote] = useState("");
+    const handleKeyPress = (keyValue) => {
+        setPreview(preview+keyValue)
+    };
+    useEffect(() => {
+        if(preview === 'forty two'){
+            fetch('https://api.quotable.io/random')
+            .then(response => response.json())
+            .then(data => {
+                setQuote(data.content);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        } else{
+            setQuote('');
+        }
+    }, [preview])
+
   return (
     <div className="keyboard">
       <div className="preview">{preview}</div>
       <div>
         {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}
-          onClick ={() => handleKeyPress(key)
-          >
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`} onClick={() => handleKeyPress(key)}>
             {key === " " ? "Space" : key.toUpperCase()}
           </button>
         ))}
